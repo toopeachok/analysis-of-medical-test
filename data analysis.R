2748d062a29c223c61f9643a81e95240e0e432b5
toopeachok <2peachok@gmail.com>
2021-04-21 02:46:06 +0300

    Add statistic about disease

--- data analysis.R
+++ data analysis.R
@@ -5,13 +5,13 @@ rowData <-
 completeTestResults = rowData %>%
   filter_at(vars(colnames(rowData[, 4:10])), all_vars(!is.na(.)))
 
-emptyWBC = rowData[which(is.na(rowData$WBC)), ]
-emptyRBC = rowData[which(is.na(rowData$RBC)), ]
-emptyHGB = rowData[which(is.na(rowData$HGB)), ]
-emptyHCT = rowData[which(is.na(rowData$HCT)), ]
-emptyPLT = rowData[which(is.na(rowData$PLT)), ]
-emptyLYM = rowData[which(is.na(rowData$LYM)), ]
-emptyMCV = rowData[which(is.na(rowData$MCV)), ]
+emptyWBC = rowData[which(is.na(rowData$WBC)),]
+emptyRBC = rowData[which(is.na(rowData$RBC)),]
+emptyHGB = rowData[which(is.na(rowData$HGB)),]
+emptyHCT = rowData[which(is.na(rowData$HCT)),]
+emptyPLT = rowData[which(is.na(rowData$PLT)),]
+emptyLYM = rowData[which(is.na(rowData$LYM)),]
+emptyMCV = rowData[which(is.na(rowData$MCV)),]
 
 # Statistics
 getmode <- function(v) {
@@ -20,23 +20,37 @@ getmode <- function(v) {
 }
 
 getStatistics <- function(data, label) {
-  sink(
-    paste(
-      "/home/toopeachok/Documents/homeworks-R/MedicalDataAnalysis/statistics.txt"
-    ),
-    TRUE
-  )
-  cat(paste("*********************"), sep = "\n")
-  cat(paste("Statistics for", label), sep = "\n")
-  cat(paste("Female quantity:", length(which(
-    data$gender == "female"
-  ))), sep = "\n")
-  cat(paste("Male quantity:", length(which(
-    data$gender == "male"
-  ))), sep = "\n")
-  cat(paste("Median of age:", median(data$age)), sep = "\n")
-  cat(paste("Mode of age:", getmode(data$age)), sep = "\n")
-  sink()
+  if (nrow(data) > 0) {
+    sink(
+      paste(
+        "/home/toopeachok/Documents/homeworks-R/MedicalDataAnalysis/statistics.txt"
+      ),
+      TRUE
+    )
+    cat(paste("*********************"), sep = "\n")
+    cat(paste("Statistics for", label), sep = "\n")
+    cat(paste("\n"))
+    cat(paste("Female quantity:", length(which(
+      data$gender == "female"
+    ))), sep = "\n")
+    cat(paste("Male quantity:", length(which(
+      data$gender == "male"
+    ))), sep = "\n")
+    cat(paste("Median of age:", median(data$age)), sep = "\n")
+    cat(paste("Mode of age:", getmode(data$age)), sep = "\n")
+    cat(paste("\n"))
+    sink()
+    typeTable = as.data.frame(table(data$Type))
+    colnames(typeTable) = c("Type", "Frequency")
+    write.table(
+      typeTable,
+      file = "/home/toopeachok/Documents/homeworks-R/MedicalDataAnalysis/statistics.txt",
+      append = TRUE,
+      row.names = FALSE,
+      col.names = TRUE,
+      sep = "\t"
+    )
+  }
 }
 
 getStatistics(completeTestResults, "Complete test results")
