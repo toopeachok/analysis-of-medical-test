b8ad542336793c1877d7d038706f6f6c8a7610a2
toopeachok <2peachok@gmail.com>
2021-06-02 00:52:07 +0300

    Add Comparison With Norms

--- data analysis.R
+++ data analysis.R
@@ -1,121 +1,47 @@
 library(ggplot2)
 library(dplyr)
-rowData = read.csv(file = "test_results.csv")
+rawData = read.csv(file = "test_results.csv")
 
-analysisTypes = colnames(rowData[, 4:10])
+analysisTypes = colnames(rawData[, 4:10])
 
-completeTestResults = rowData %>%
-  filter_at(vars(all_of(analysisTypes)), all_vars(!is.na(.)))
-
-# Statistics
-getmode = function(v) {
-  uniqv = unique(v)
-  uniqv[which.max(tabulate(match(v, uniqv)))]
-}
-
-getStatistics = function(rowData,
-                         analysisType,
-                         statisticsFileName = "statistics.txt") {
-  data = rowData[which(is.na(rowData[[analysisType]])),]
-  
-  if (nrow(data) > 0) {
-    resultStr = paste("*********************", "\n", sep = "")
-    resultStr = paste(resultStr, "Statistics for ", analysisType, "\n", sep = "")
-    cat(resultStr, file = statisticsFileName, append = TRUE)
-    resultStr = ""
-    genderStatistics = as.data.frame(table(data$gender))
-    colnames(genderStatistics) = c("Gender", "Frequency")
-    write.table(
-      genderStatistics,
-      file = statisticsFileName,
-      append = TRUE,
-      quote = FALSE,
-      row.names = FALSE,
-      col.names = TRUE,
-      sep = "\t"
-    )
-    resultStr = paste(resultStr, "Median of age: ", median(data$age), "\n", sep = "")
-    resultStr = paste(resultStr, "Mode of age: ", getmode(data$age), "\n", sep = "")
-    # Append Results To Text File
-    cat(resultStr, file = statisticsFileName, append = TRUE)
-    # Disease Statistics
-    typeTable = as.data.frame(table(data$Type))
-    colnames(typeTable) = c("Type", "Frequency")
-    # Append Disease Statistics To Text File
-    write.table(
-      typeTable,
-      file = statisticsFileName,
-      append = TRUE,
-      quote = FALSE,
-      row.names = FALSE,
-      col.names = TRUE,
-      sep = "\t"
-    )
-  }
-}
-
-statisticsFileName = "statistics.txt"
-if (file.exists(statisticsFileName)) {
-  file.remove(statisticsFileName)
-}
-
-for (analysisType in analysisTypes) {
-  getStatistics(rowData, analysisType)
-}
-
-# Plots
-drawHist = function(rowData, analysisType) {
-  data = rowData[which(is.na(rowData[[analysisType]])),]
-  if (nrow(data) > 0) {
-    hist(data$age,
-         main = paste("Histogram for", analysisType),
-         xlab = "age")
-  }
-}
-
-pdf("plots.pdf")
-hist(completeTestResults$age, main = "Histogram for complete test results", xlab = "age")
-for (analysisType in analysisTypes) {
-  drawHist(rowData, analysisType)
-}
-
-dev.off()
-
-# Filled Data
-fillData = function(rowData, analysisType) {
-  filledData = data.frame(rowData)
-  for (i in row.names(rowData[which(is.na(rowData[[analysisType]])),])) {
-    sampleData = rowData[which(
-      !is.na(rowData[[analysisType]]) &
-        abs(rowData$age - rowData[i,]$age) <= 5 &
-        rowData$gender == rowData[i, ]$gender &
-        rowData$Type == rowData[i, ]$Type
+# Fill data
+fillData = function(rawData, analysisType) {
+  filledData = data.frame(rawData)
+  for (i in row.names(rawData[which(is.na(rawData[[analysisType]])),])) {
+    sampleData = rawData[which(
+      !is.na(rawData[[analysisType]]) &
+        abs(rawData$age - rawData[i,]$age) <= 5 &
+        rawData$gender == rawData[i, ]$gender &
+        rawData$Type == rawData[i, ]$Type
     ), ]
     if (nrow(sampleData) > 0) {
-      filledData[i, ][[analysisType]] = median(sampleData[[analysisType]])
+      filledData[i, analysisType] = median(sampleData[[analysisType]])
     }
   }
   
   return(filledData)
 }
 
-filledData = data.frame(rowData)
+filledData = data.frame(rawData)
 for (analysisType in analysisTypes) {
   filledData = data.frame(fillData(filledData, analysisType))
 }
 
 write.csv(filledData, "filled_test_results.csv", row.names = FALSE)
 
-# Comparison With Norms
+# Comparison with norms
 norms = read.csv(file = "norms.csv")
-# To get proper structure we can just copy filled data
-# Later we will put actual delta values
+# To get proper structure we can just copy filled data,
+# later we will put actual delta values:
+# zero if value in norm,
+# value - upper_norm_value, if excess (will be positive num),
+# value - bottom_norm_value, if reduction (will be negative num)
 deltaNorms = data.frame(filledData)
 
 getNormDelta = function(norms, analysisType, value) {
   delta = 0
-  # norms is a data frame with two rows
-  # norms[2, ] upper limit of the norm
+  # norms is a data frame with two rows:
+  # norms[2, ] upper limit of the norm,
   # norms[1, ] bottom
   if (value > norms[2, analysisType]) {
     delta  = value - norms[2, analysisType]
@@ -125,6 +51,7 @@ getNormDelta = function(norms, analysisType, value) {
   return(delta)
 }
 
+# Filling of deltaNorms
 for (i in row.names(deltaNorms)) {
   for (analysisType in analysisTypes) {
     delta = NA
@@ -136,3 +63,62 @@ for (i in row.names(deltaNorms)) {
     deltaNorms[i, analysisType] = delta
   }
 }
+
+# Statistics and plots for each disease according to deltas of norms
+diseases = unique(deltaNorms[["Type"]])
+plotsReductionNorms = vector(length(diseases), mode = 'list')
+plotsExcessNorms = vector(length(diseases), mode = 'list')
+
+for (i in 1:length(diseases)) {
+  disease = diseases[[i]]
+  # diseaseData is a data frame with two rows and length(analysisTypes) columns.
+  # Columns are a analysis types,
+  # rows contains frequency of
+  # excess of norms - diseaseData[2, ],
+  # reduction of norms - diseaseData[1, ]
+  diseaseData = as.data.frame(matrix(0, 2, length(analysisTypes)))
+  colnames(diseaseData) = analysisTypes
+  
+  for (analysisType in analysisTypes) {
+    reduction = nrow(deltaNorms[which(deltaNorms[[analysisType]] < 0 &
+                                        deltaNorms[["Type"]] == disease), ])
+    excess = nrow(deltaNorms[which(deltaNorms[[analysisType]] > 0 &
+                                     deltaNorms[["Type"]] == disease), ])
+    diseaseData[1, analysisType] = reduction
+    diseaseData[2, analysisType] = excess
+  }
+  
+  dotchart(
+    as.numeric(diseaseData[1, ]),
+    labels = colnames(diseaseData),
+    main = disease,
+    xlab = "Frequency of reduction norms",
+    color = "blue"
+  )
+  plotsReductionNorms[[i]] = recordPlot()
+  graphics.off()
+  
+  dotchart(
+    as.numeric(diseaseData[2, ]),
+    labels = colnames(diseaseData),
+    main = disease,
+    xlab = "Frequency of excess norms",
+    color = "red"
+  )
+  plotsExcessNorms[[i]] = recordPlot()
+  graphics.off()
+}
+
+pdf("plotsReductionNorms.pdf", onefile = TRUE)
+for (plot in plotsReductionNorms) {
+  replayPlot(plot)
+}
+graphics.off()
+
+pdf("plotsExcessNorms.pdf", onefile = TRUE)
+for (plot in plotsExcessNorms) {
+  replayPlot(plot)
+}
+graphics.off()
+
+write.csv(deltaNorms, "deltas_of_norms.csv", row.names = FALSE)
